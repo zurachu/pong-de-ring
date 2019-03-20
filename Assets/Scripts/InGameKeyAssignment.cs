51bcb50c77c137d2568ee2e2af682e44f8d0992f
zurachu <zurachu@gmail.com>
2019-03-21 01:19:02 +0900

    一部環境でテンキーが効かない対応として、Keypad1〜9の押下チェックではAlpha1〜9もチェックする

--- Assets/Scripts/InGameKeyAssignment.cs
+++ Assets/Scripts/InGameKeyAssignment.cs
@@ -5,6 +5,19 @@ using UnityEngine;
 
 public class InGameKeyAssignment : MonoBehaviour
 {
+    static readonly Dictionary<KeyCode, KeyCode> keypadNumberKeyMap = new Dictionary<KeyCode, KeyCode> {
+        { KeyCode.Keypad0, KeyCode.Alpha0 },
+        { KeyCode.Keypad1, KeyCode.Alpha1 },
+        { KeyCode.Keypad2, KeyCode.Alpha2 },
+        { KeyCode.Keypad3, KeyCode.Alpha3 },
+        { KeyCode.Keypad4, KeyCode.Alpha4 },
+        { KeyCode.Keypad5, KeyCode.Alpha5 },
+        { KeyCode.Keypad6, KeyCode.Alpha6 },
+        { KeyCode.Keypad7, KeyCode.Alpha7 },
+        { KeyCode.Keypad8, KeyCode.Alpha8 },
+        { KeyCode.Keypad9, KeyCode.Alpha9 },
+    };
+
     [Serializable]
     public class KeyInfo
     {
@@ -13,6 +26,18 @@ public class InGameKeyAssignment : MonoBehaviour
     }
 
     [SerializeField] List<KeyInfo> keyInfos;
+
+    public bool GetKeyUp(int index)
+    {
+        if (!IsValidIndex(index))
+        {
+            return false;
+        }
+
+        var code = Code(index);
+        return Input.GetKeyUp(code)
+            || (keypadNumberKeyMap.ContainsKey(code) && Input.GetKeyUp(keypadNumberKeyMap[code]));
+    }
 
     public KeyCode Code(int index)
     {
